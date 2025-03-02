483e9ef55277a6ed591f28fdf30891f52a27d99a
Nakin Ahluwalia <nakinahluwalia@mac.mynetworksettings.com>
2025-03-02 10:32:07 -0500

    1 day break but we're so back

---
+++ neet/neet150/two_sum/two_sum.py
@@ -0,0 +1,19 @@
+# Input: 
+# nums = [3,4,5,6], target = 7
+
+# Output: [0,1]
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1, n):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+        return[]       
+
+sol = Solution()
+nums = [3,4,5,6]
+target = 7
+result = sol.twoSum(nums, target)
+print(result)
