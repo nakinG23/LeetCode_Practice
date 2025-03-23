79cfed395bb679360e7cc73127a2f85d19d7d7c8
Nakin Ahluwalia <89446480+nakinG23@users.noreply.github.com>
2025-03-23 13:41:44 -0400

    Best day ever (#13)
    Co-authored-by: Nakin Ahluwalia <nakinahluwalia@gmail.com>

--- NeetCode/neet150/3_Sum/sol2.py
+++ NeetCode/neet150/3_Sum/sol2.py
@@ -20,13 +20,8 @@ class Solution:
                     if triplet not in index_list:
                         index_list.append(triplet)
 
-                    # Skip duplicates for left and right
                     left += 1
                     right -= 1
-                    # while left < right and nums[left] == nums[left - 1]:
-                    #     left += 1
-                    # while left < right and nums[right] == nums[right + 1]:
-                    #     right -= 1
 
                 elif total < 0:
                     left += 1
