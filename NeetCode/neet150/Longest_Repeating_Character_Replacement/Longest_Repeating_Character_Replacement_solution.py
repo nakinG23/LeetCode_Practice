fc0e138fe146be7de2615d7fddf16444ad2c0775
Nakin Ahluwalia <nakinahluwalia23@gmail.com>
2025-03-25 22:13:21 -0400

    good day's work

---
+++ NeetCode/neet150/Longest_Repeating_Character_Replacement/Longest_Repeating_Character_Replacement_solution.py
@@ -0,0 +1,20 @@
+from typing import List
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
+        res = 0
+        count = {}
+
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            
+            if (r-l+1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
+
+            res = max(res, r-l+1)
+        return res
+
+sol = Solution()
+result = sol.characterReplacement(s="ABABBA",k=2)
+print(result)
