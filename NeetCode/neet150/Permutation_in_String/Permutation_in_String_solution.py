d1095b83895bed874f7db17d00e2432b33fa698a
Nakin Ahluwalia <nakinahluwalia23@gmail.com>
2025-03-27 22:57:40 -0400

    Finally! Onlogn solution and On solution

--- NeetCode/neet150/Permutation_in_String/Permutation_in_String_solution.py
+++ NeetCode/neet150/Permutation_in_String/Permutation_in_String_solution.py
@@ -1,6 +1,8 @@
 from typing import List
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
         seen = list()
         n1 = len(s1)
         n2 = len(s2)
@@ -9,9 +11,11 @@ class Solution:
             seen.append(s2[r])
             if r > n1-2:
                 while count < n1:
-                    if s1[count] in seen:
-                        count += 1
-                        print("seen = ", seen)
+                    if sorted(s1) == sorted(seen):
+                        return True
+                    # if s1[count] in seen:
+                    #     count += 1
+                    #     print("seen = ", seen)
                     else:
                         count = 0
                         seen.pop(0)
@@ -21,7 +25,7 @@ class Solution:
         else:
             return False
 
-
+#  works perfectly but cant be said to be on O(n) time
 sol = Solution()
 result = sol.checkInclusion(s1="hello", s2="ooolleoooleh")
 print(result)
