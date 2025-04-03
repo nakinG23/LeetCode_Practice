62d3387e5672b6cdd5478ce121ee0ea09290964a
Nakin Ahluwalia <nakinahluwalia23@gmail.com>
2025-04-02 23:22:46 -0400

    finally done with sliding window

--- NeetCode/neet150/Minimum_window_substring/Minimum_window_substring_solution.py
+++ NeetCode/neet150/Minimum_window_substring/Minimum_window_substring_solution.py
@@ -1,57 +1,38 @@
 from typing import List
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s):
+        if t == "":
             return ""
-        charfreq_t = [0] * 52
-        charfreq_window = [0] * 52
-        n1 = len(t)
-        n2 = len(s)
-        t_list = list(t)
-        return_char = ""
-        return_char_list = list()
-        max_return_char_len = 0
-        for c in t:
-                if c.islower():
-                    charfreq_t[ord(c) - ord('a')] += 1
-                else:
-                    charfreq_t[ord(c) - ord('A') + 26] += 1
 
-        for i in range(n2):
-                if s[i].islower():
-                    charfreq_window[ord(s[i]) - ord('a')] += 1
-                else:
-                    charfreq_window[ord(s[i]) - ord('A') + 26] += 1
-                
-                if s[i] in t:
-                    k = i
-                    while t_list and k<n2:
-                        if s[k] in t_list:
-                            print("about to remove ", s[k], "from", t_list)
-                            t_list.remove(s[k])
-                        return_char += s[k]
-                        k += 1
-                    print("return char = ", return_char)
-                    if len(return_char) >= n1: 
-                        return_char_list.append(return_char)
-                    t_list = list(t)
-                    return_char = ""
-                    print(return_char_list)
+        t_count, win = {}, {}
+        for char in t:
+            t_count[char] = 1 + t_count.get(char, 0)
+        
+        print("t_count: ", t_count)
+
+        have, need = 0, len(t_count)
+        print("len t_count: ", need)
+        res, resLen = [-1, -1], float("infinity")
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+            win[c] = 1 + win.get(c, 0)
+
+            if c in t_count and win[c] == t_count[c]:
+                have += 1
+
+            while have == need:
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = r - l + 1
                     
-                # max_return_char_len = max(max_return_char_len, len(charfreq_window))    
-        return return_char
-
-
-                    # if (a >= b for a, b in zip(t1, t2))
-
-                    # if i >= n1:
-                    #     if s[i].islower():
-                    #         charfreq_window[ord(s[i - n1]) - ord('a')] -= 1
-                    #     else:
-                    #         charfreq_window[ord(s[i - n1]) - ord('A') + 26] -= 1
-
-        print(charfreq_t)
-
+                win[s[l]] -= 1
+                if s[l] in t_count and win[s[l]] < t_count[s[l]]:
+                    have -= 1
+                l += 1
+                print(s[l : r + 1])
+        l, r = res
+        return s[l : r + 1] if resLen != float("infinity") else ""
 
 sol = Solution()
 result = sol.minWindow(s="OUZODYXAZV", t="XYZ")
