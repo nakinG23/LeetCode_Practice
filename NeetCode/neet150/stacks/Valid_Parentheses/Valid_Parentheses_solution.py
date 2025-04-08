6e163f32d5ea590324bb3bfebc3a7b2f2cff8826
Nakin Ahluwalia <nakinahluwalia23@gmail.com>
2025-04-08 02:20:25 -0400

    we're so back

--- NeetCode/neet150/stacks/Valid_Parentheses/Valid_Parentheses_solution.py
+++ NeetCode/neet150/stacks/Valid_Parentheses/Valid_Parentheses_solution.py
@@ -1,1 +1,26 @@
 from typing import List
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s)<2:
+            return False
+        stack = []
+        for i in range(len(s)):
+            if s[i]=="{":
+                stack.append(s[i])
+            elif s[i]=="(":
+                stack.append(s[i])
+            elif s[i]=="[":
+                stack.append(s[i])
+            elif stack and s[i]=="]" and stack[-1] == "[" and i!=0:
+                stack.pop()
+            elif stack and s[i]==")" and stack[-1] == "(" and i!=0:
+                stack.pop()
+            elif stack and s[i]=="}" and stack[-1] == "{" and i!=0:
+                stack.pop()
+            else:
+                stack.append(s[i])
+        print(stack)
+        return not stack
+
+sol = Solution()
+res = sol.isValid(s="]]")
