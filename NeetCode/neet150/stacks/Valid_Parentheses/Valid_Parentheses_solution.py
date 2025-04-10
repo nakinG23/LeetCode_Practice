1c0e03cce553455ec7e1d33c0c75df57310df32a
Nakin Ahluwalia <89446480+nakinG23@users.noreply.github.com>
2025-04-10 00:25:25 -0400

    day25 (#21)
    * adding folders so i can re org (#19)
    
    * Day24 (#20)
    
    * adding folders so i can re org
    
    * we're so back

--- NeetCode/neet150/stacks/Valid_Parentheses/Valid_Parentheses_solution.py
+++ NeetCode/neet150/stacks/Valid_Parentheses/Valid_Parentheses_solution.py
@@ -1,26 +1,25 @@
-from typing import List
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s)<2:
+        if len(s) < 2:
             return False
         stack = []
         for i in range(len(s)):
-            if s[i]=="{":
+            if s[i] == "{":
                 stack.append(s[i])
-            elif s[i]=="(":
+            elif s[i] == "(":
                 stack.append(s[i])
-            elif s[i]=="[":
+            elif s[i] == "[":
                 stack.append(s[i])
-            elif stack and s[i]=="]" and stack[-1] == "[" and i!=0:
+            elif stack and s[i] == "]" and stack[-1] == "[":
                 stack.pop()
-            elif stack and s[i]==")" and stack[-1] == "(" and i!=0:
+            elif stack and s[i] == ")" and stack[-1] == "(":
                 stack.pop()
-            elif stack and s[i]=="}" and stack[-1] == "{" and i!=0:
+            elif stack and s[i] == "}" and stack[-1] == "{":
                 stack.pop()
             else:
-                stack.append(s[i])
-        print(stack)
+                return False
         return not stack
 
 sol = Solution()
 res = sol.isValid(s="]]")
+print(res)
