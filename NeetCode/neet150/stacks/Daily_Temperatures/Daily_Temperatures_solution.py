284654c18ea01adb5ba662b494ec2a82656ddcf1
Nakin Ahluwalia <89446480+nakinG23@users.noreply.github.com>
2025-04-15 21:32:56 -0400

    Day30 (#26)
    * slow
    
    * temp
    
    * stacks are legit

--- NeetCode/neet150/stacks/Daily_Temperatures/Daily_Temperatures_solution.py
+++ NeetCode/neet150/stacks/Daily_Temperatures/Daily_Temperatures_solution.py
@@ -1,20 +1,31 @@
 from typing import List
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # res = [0 for _ in range(len(temperatures))]
+        # for i in range(len(temperatures)-1):
+        #     for j in range(i+1, len(temperatures)):
+        #         if temperatures[j] > temperatures[i]:
+        #             res[i] = j-i
+        #             break
+        # return res
         res = [0 for _ in range(len(temperatures))]
-        for i in range(len(temperatures)-1):
-            for j in range(i+1, len(temperatures)):
-                if temperatures[j] > temperatures[i]:
-                    res[i] = j-i
-                    break
+        stack = [] # to store temp and its index
+
+        for i, temp in enumerate(temperatures):
+            while stack and temp > stack[-1][1]:
+                indStack, tempStack = stack.pop()
+                res[indStack] = (i - indStack)
+            stack.append([i, temp])
         return res
 
-
-
+sol = Solution()
+print(sol.dailyTemperatures(temperatures=[30,38,30,36,35,40,28]))
 
 '''
 temps = [30,38,30,36,35,40,28]
              i  
+stack = [[30,1],[38,2],[30,3],]
+res   = [1,0,0,0,0,0,0]
 for i keep adding to stack if temp is < ith temp
 so if 38, add 30, 36, 35, 40 -> we pop
 resul = [1,]
