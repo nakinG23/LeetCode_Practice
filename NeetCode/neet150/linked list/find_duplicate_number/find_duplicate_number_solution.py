6c91677625fadd9c3ff85e585e7cc73a9cc6325d
Nakin Ahluwalia <nakinahluwalia23@gmail.com>
2025-09-09 14:47:39 -0400

    added array soln

--- NeetCode/neet150/linked list/find_duplicate_number/find_duplicate_number_solution.py
+++ NeetCode/neet150/linked list/find_duplicate_number/find_duplicate_number_solution.py
@@ -16,3 +16,13 @@ class Solution:
                 return num
             seen.add(num)
         return -1
+    
+# array:
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        seen = [0] * len(nums)
+        for num in nums:
+            if seen[num - 1]:
+                return num
+            seen[num - 1] = 1
+        return -1
