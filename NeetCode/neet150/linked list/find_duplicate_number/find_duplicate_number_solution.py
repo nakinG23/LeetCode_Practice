4dc16e02140915b11cced1dc586350570571a504
Nakin Ahluwalia <nakinahluwalia23@gmail.com>
2025-09-09 14:46:59 -0400

    wow day3 already :wq
    exit

---
+++ NeetCode/neet150/linked list/find_duplicate_number/find_duplicate_number_solution.py
@@ -0,0 +1,18 @@
+# # solution 1:
+# class Solution:
+#     def findDuplicate(self, nums: List[int]) -> int:
+#         nums.sort()
+#         for i in range(len(nums)-1):
+#             if nums[i]==nums[i+1]:
+#                 return nums[i]
+# Solution 2:
+from typing import List
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return num
+            seen.add(num)
+        return -1
