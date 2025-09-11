19df2ceed87f549387b4c4040eb9b2f044f417c4
Nakin Ahluwalia <89446480+nakinG23@users.noreply.github.com>
2025-09-10 23:58:19 -0400

    Day4 (#48)
    * wow day3 already
    :wq
    
    exit
    
    * added array soln
    
    * quick

--- NeetCode/neet150/linked list/find_duplicate_number/find_duplicate_number_solution.py
+++ NeetCode/neet150/linked list/find_duplicate_number/find_duplicate_number_solution.py
@@ -25,4 +25,14 @@ class Solution:
             if seen[num - 1]:
                 return num
             seen[num - 1] = 1
+
+    
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return num
+            seen.add(num)
         return -1
+   
