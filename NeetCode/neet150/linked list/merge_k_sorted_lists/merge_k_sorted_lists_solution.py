b86de487c8b7f4eb039b683a2908e71f795ea8ad
Nakin Ahluwalia <89446480+nakinG23@users.noreply.github.com>
2025-09-13 00:29:40 -0400

    2 in a day woohoo (#51)

---
+++ NeetCode/neet150/linked list/merge_k_sorted_lists/merge_k_sorted_lists_solution.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        nodes = []
+        for lst in lists:
+            while lst:
+                nodes.append(lst.val)
+                lst = lst.next
+        nodes.sort()
+
+        res = ListNode(0)
+        cur = res
+        for node in nodes:
+            cur.next = ListNode(node)
+            cur = cur.next
+        return res.next
